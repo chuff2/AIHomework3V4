d8502973fbdafde2b8afdc8dd873a5080d15dc49
Tiffany Voss <voss@macaroni-09.cs.wisc.edu>
2016-03-05 19:57:26 -0600

    added parent setter fcn

--- src/DecTreeNodeImpl.java
+++ src/DecTreeNodeImpl.java
@@ -6,4 +6,12 @@ public class DecTreeNodeImpl extends DecTreeNode {
 		// TODO Auto-generated constructor stub
 	}
 
+	public void addChild(DecTreeNode child) {
+		super(child);
+	}
+
+	public void setParentValue(String _parentAttrVal){
+		this.parentAttributeValue = _parentAttrVal;
+	}
+
 }
